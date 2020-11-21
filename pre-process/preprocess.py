895a7115e5a1263dacd83288254b024ef7a3fbae
Caio Davi <caio.davi@tamu.edu>
2020-11-21 15:46:55 -0600

    script to create labels from annotations

--- pre-process/preprocess.py
+++ pre-process/preprocess.py
@@ -1,14 +1,16 @@
+import os
 from skimage import data
-from skimage.io import imsave, imread
+from skimage.io import imsave, imread, imshow
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
 
 ORIGINAL = "./originals/100x100_1.png"
-TRAIN_PATH = "./p20/train/images/"
+TRAIN_PATH = "./p20/train/"
 TEST_PATH = "./p20/test/"
 
 SAMPLE_IMAGE = "./p20/train/images/1.png"
 SAMPLE_LABEL = "./p20/train/labels/0.png"
+SAMPLE_ANOTATION = "./p20/train/anotations/0.png"
 
 
 def split_image(image_path, save_path, splited_dimensions):
@@ -22,19 +24,23 @@ def split_image(image_path, save_path, splited_dimensions):
 
 
 # Spliting Original Image:
-split_image(ORIGINAL, TRAIN_PATH, (512, 512))
+# split_image(ORIGINAL, TRAIN_PATH, (512, 512))
 
 
-def gaussian(image_path):
-    image = imread(image_path)
-
-    # label = np.array(Image.open(label_path))
-    label = imread(image_path)
-    label = 100.0 * (label[:, :, 0] > 0)
-    label = gaussian_filter(label, sigma=(1, 1), order=0)
-
-    plt.imshow(label, cmap='gray')
+def saveBW(image_name, image_path, label_path):
+    label = imread(image_path+image_name)
+    label = 255 * (label[:, :, 1] > 0)
+    plt.imshow(label)
     plt.show()
+    imsave(label_path+image_name, label)
 
 
-# gaussian(SAMPLE_LABEL)
+def createLabels(anotation_path, label_path):
+    a = os.listdir(anotation_path)
+    for img in a:
+        saveBW(img, anotation_path, label_path)
+    print(a)
+
+
+# saveBW("1.png", TRAIN_PATH+"anotations/", TRAIN_PATH+"labels/")
+createLabels(TRAIN_PATH+"anotations/", TRAIN_PATH+"labels/")
